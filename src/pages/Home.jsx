73052bbb06aedd540e3f25ca20646ef0829146d4
samuel habtamu <152323187+Samuelgit3919@users.noreply.github.com>
2025-02-09 17:09:40 +0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+import Footer from "../components/footer/Footer";
+import Header from "../components/header/Header";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
