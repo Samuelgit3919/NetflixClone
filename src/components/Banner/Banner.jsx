315cdf89366578d2bdf23608913f792c689177d4
samuel habtamu <152323187+Samuelgit3919@users.noreply.github.com>
2025-02-14 16:20:50 +0300

    commit

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "../utils/axios";
 import requests from "../utils/requests";
-
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import "./Banner.css";
 
 const Banner = () => {
@@ -21,12 +22,16 @@ const Banner = () => {
       }
     })();
   }, []);
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
   return (
     <div
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
       }}
@@ -35,13 +40,13 @@ const Banner = () => {
         <h1 className="banner__title">
           {movie?.title || movie?.name || movie?.original_name}
         </h1>
-        <div className="banner__buttons">
+        <div className="banner__button">
           <button className="banner__button play">Play</button>
-          <button className="banner__button ">List</button>
+          <button className="banner__button info ">My List</button>
         </div>
-        {/* <h1 className="banner__description">
+        <h1 className="banner__description">
           {truncate(movie?.overview, 150)}
-        </h1> */}
+        </h1>
       </div>
       <div className="banner__fadeBottom" />
     </div>
