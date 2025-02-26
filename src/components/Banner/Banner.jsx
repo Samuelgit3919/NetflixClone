6035dc8ac58508e8d7e809c72d3e08dd0e5072e4
samuel habtamu <152323187+Samuelgit3919@users.noreply.github.com>
2025-02-26 17:48:17 +0300

    commit

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -32,6 +32,7 @@ const Banner = () => {
         backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
+        height: "100vh",
       }}
     >
       <div className="banner__contents">
